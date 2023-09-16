6118457689d83831d96dac60ae2929ba4c47d8c2
Saurabh Kishore <gks210199@gmail.com>
2023-09-16 02:00:33 -0400

    tuesday challenge complete

--- tuesday.py
+++ tuesday.py
@@ -1,7 +1,18 @@
 def factorial(num):
     product = 1
-    for i in range(num):
+    for i in range(1,num):
         product = product * i
     return product
+def reverse(str):
+    return str[::-1]
 
-print(factorial(10))
+fact = str(factorial(34234))
+
+numOfZeros = 0
+for digit in reverse(fact):
+    if (digit == "0"):
+        numOfZeros += 1
+    else:
+        break
+print(numOfZeros)
+
